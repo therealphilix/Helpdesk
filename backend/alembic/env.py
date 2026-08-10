2514513f7657cbc2d300bdf5ae4e5d5287fa3bcc
Uchenna Felix <uchennafelix888@gmail.com>
2026-08-10 15:17:12 +0100

    fix migration issue

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -11,9 +11,13 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from app.core.config import settings
+
 config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 from app.models import Base
 
